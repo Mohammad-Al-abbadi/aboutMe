e95d47fc87c37fa40e6d8d5def0122261af9e6ec
Mohammad-Al-abbadi <mohammadalabbadi1996@gmail.com>
2021-05-23 17:14:27 +0300

    changes to the code

--- aboutMe.js
+++ aboutMe.js
@@ -1,45 +1,45 @@
 
 let txt;
-let person = prompt("Please enter your name:", "Harry Potter");
+let person = prompt("Please enter your name:");
 if (person == null || person == "") {
     txt = "User cancelled the prompt.";
 } else {
     txt = "Hello " + person + "! How are you today?";
 }
-document.getElementById("Name").innerHTML = txt;
+alert("your name is:", person);
+//document.getElementById("Name").innerHTML = txt;
 
-prompt("enter your age: ");
-function Age() {
-    let txt;
-    let Age = prompt("Please enter your Age:");
-    if (Age == null || Age == "") {
-        txt = "User cancelled the prompt.";
-    } else {
-        txt = "your age: " + Age;
-    }
-    alert("your age is:", "Age");
+
+let txt;
+let Age = prompt("Please enter your Age:");
+if (Age == null || Age == "") {
+    txt = "User cancelled the prompt.";
+} else {
+    txt = "your age: " + Age;
+
+    alert("your age is:", Age);
     //document.getElementById("Age").innerHTML = txt;
 }
 
-function myFunction() {
-    let txt;
-    let person = prompt("Please enter your name:", "Harry Potter");
-    if (person == null || person == "") {
-        txt = "User cancelled the prompt.";
-    } else {
-        txt = "Hello " + person + "! How are you today?";
-    }
-    document.getElementById("demo").innerHTML = txt;
-}
 
-function myFunction() {
-    let txt;
-    let person = prompt("Please enter your name:", "Harry Potter");
-    if (person == null || person == "") {
-        txt = "User cancelled the prompt.";
-    } else {
-        txt = "Hello " + person + "! How are you today?";
-    }
-    document.getElementById("demo").innerHTML = txt;
+let txt;
+let movie = prompt("Please enter your favorite movie:");
+if (movie == null || movie == "") {
+    txt = "User cancelled the prompt.";
+} else {
+    txt = "your favorite movie: " + movie;
 }
+//document.getElementById("demo").innerHTML = txt;
+
+
+
+let txt;
+let country = prompt("Please enter your country");
+if (country == null || country == "") {
+    txt = "User cancelled the prompt.";
+} else {
+    txt = "your country is: " + country;
+}
+    //document.getElementById("demo").innerHTML = txt;
+
 
